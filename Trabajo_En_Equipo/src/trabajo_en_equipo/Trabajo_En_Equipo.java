8eecbf78390213cc21e8eac9c612db6427d8336e
Jorge <Jorge@Galleon-A301856>
2021-06-01 16:52:47 -0500

    Tercera Versión manejo de listas...

--- Trabajo_En_Equipo/src/trabajo_en_equipo/Trabajo_En_Equipo.java
+++ Trabajo_En_Equipo/src/trabajo_en_equipo/Trabajo_En_Equipo.java
@@ -7,31 +7,10 @@ public class Trabajo_En_Equipo {
 
     public static void main(String[] args) {
         lista_sencilla l1=new lista_sencilla();
+        //Se insertan dos datos en una lista sencilla
         l1.insertar(1, 2);
+        //Impresión de la lista
         l1.imprimir();
-//        System.out.println("Hola mundo");
-//        //Ejercicio basico de listas...
-//        nodo cab;
-//        nodo q;
-//        cab = new nodo(1);
-//        q = new nodo (2);
-//        cab.sig = q;
-//        nodo aux = cab;
-//        System.out.println("Impresion de la lista: ");
-//        while(aux != null){
-//            System.out.println(aux.info);
-//            aux = aux.sig;
-//        }
-//        System.out.println("_________");
-//        q = new nodo (3);
-//        cab.sig.sig = q;
-//        q.sig = null;
-//        aux = cab;
-//        System.out.println("Impresion de la lista: ");
-//        while(aux != null){
-//            System.out.println(aux.info);
-//            aux = aux.sig;
-//        }
     }
     
 }
