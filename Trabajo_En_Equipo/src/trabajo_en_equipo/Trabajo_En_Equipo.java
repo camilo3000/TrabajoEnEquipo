752722457c77d8417dfd7f44d3811e6d4ef7a268
Camilo Fabian Uriana <Camilo Fabian Uriana@DESKTOP-N9TIH11>
2021-06-01 16:05:38 -0500

    Nuevo Trabajo En Equipo

---
+++ Trabajo_En_Equipo/src/trabajo_en_equipo/Trabajo_En_Equipo.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package trabajo_en_equipo;
+
+/**
+ *
+ * @author Camilo Fabian Uriana
+ */
+public class Trabajo_En_Equipo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
