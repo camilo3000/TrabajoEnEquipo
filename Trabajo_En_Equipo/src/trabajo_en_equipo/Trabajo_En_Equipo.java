31bda8cf6f90e40b017f6525321a3c847bb8ad09
Jorge <Jorge@Galleon-A301856>
2021-06-01 16:18:26 -0500

    Primera Versión manejo de listas...

--- Trabajo_En_Equipo/src/trabajo_en_equipo/Trabajo_En_Equipo.java
+++ Trabajo_En_Equipo/src/trabajo_en_equipo/Trabajo_En_Equipo.java
@@ -1,21 +1,42 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package trabajo_en_equipo;
 
-/**
- *
- * @author Camilo Fabian Uriana
- */
+
 public class Trabajo_En_Equipo {
 
-    /**
-     * @param args the command line arguments
-     */
+
     public static void main(String[] args) {
         System.out.println("Hola mundo");
+        //Ejercicio basico de listas...
+        nodo cab;
+        nodo q;
+        cab = new nodo(1);
+        q = new nodo (2);
+        cab.sig = q;
+        nodo aux = cab;
+        System.out.println("Impresion de la lista: ");
+        while(aux != null){
+            System.out.println(aux.info);
+            aux = aux.sig;
+        }
+        System.out.println("_________");
+        q = new nodo (3);
+        cab.sig.sig = q;
+        q.sig = null;
+        aux = cab;
+        System.out.println("Impresion de la lista: ");
+        while(aux != null){
+            System.out.println(aux.info);
+            aux = aux.sig;
+        }
     }
     
+}
+
+class nodo{
+    int info;
+    nodo (int x){
+        info = x;
+    }
+    nodo sig;
 }
