7469b932b5cce269a6bce98e64ebecbc89a8d983
Camilo Fabian Uriana <Camilo Fabian Uriana@DESKTOP-N9TIH11>
2021-06-02 14:04:17 -0500

    Nuevo Trabajo En Equipo

--- Trabajo_En_Equipo/src/trabajo_en_equipo/Trabajo_En_Equipo.java
+++ Trabajo_En_Equipo/src/trabajo_en_equipo/Trabajo_En_Equipo.java
@@ -7,10 +7,38 @@ public class Trabajo_En_Equipo {
 
     public static void main(String[] args) {
         lista_sencilla l1=new lista_sencilla();
+<<<<<<< Upstream, based on origin/master
         //Se insertan dos datos en una lista sencilla
         l1.insertar(1, 2);
         //Impresión de la lista
         l1.imprimir();
+=======
+        l1.insertar(1, 2);
+        l1.imprimir();
+//        System.out.println("Hola mundo");
+//        //Ejercicio basico de listas...
+//        nodo cab;
+//        nodo q;
+//        cab = new nodo(1);
+//        q = new nodo (2);
+//        cab.sig = q;
+//        nodo aux = cab;
+//        System.out.println("Impresion de la lista: ");
+//        while(aux != null){
+//            System.out.println(aux.info);
+//            aux = aux.sig;
+//        }
+//        System.out.println("_________");
+//        q = new nodo (3);
+//        cab.sig.sig = q;
+//        q.sig = null;
+//        aux = cab;
+//        System.out.println("Impresion de la lista: ");
+//        while(aux != null){
+//            System.out.println(aux.info);
+//            aux = aux.sig;
+//        }
+>>>>>>> f773b47 Nuevo Trabajo En Equipo
     }
     
 }
